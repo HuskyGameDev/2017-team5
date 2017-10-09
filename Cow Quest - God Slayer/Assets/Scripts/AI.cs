71cdf1d67303756782713b911a3074c76b8b0f06
cwalker97 <collinw@mtu.edu>
2017-10-08 20:17:25 -0400

    made some scripts, may work

---
+++ Cow Quest - God Slayer/Assets/Scripts/AI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    public GameObject enemy;
+    public GameObject hero;
+    private float maxSpeed = 5f;
+    private float moveForce = 350f;
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        hero = GameObject.FindGameObjectWithTag("Hero");
+      bool detected = Detection();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    bool Detection()
+    {
+        if (Mathf.Abs(hero.transform.position.x - enemy.transform.position.x) < 30)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
