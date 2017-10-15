964cfc934cf5e28c6ba77e5c50f1b16e9dd818da
hikari1734 <mlwalthe@mtu.edu>
2017-10-15 17:21:43 -0400

    cube

---
+++ Cow Quest - God Slayer/Assets/Scripts/Ground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour {
+
+    public GameObject ground;
+    public float verticalAxis = 0f;
+    public float horizontalAxis = 0;
+
+    private Vector2 origin;
+
+	// Use this for initialization
+	void Start () {
+        origin = transform.position;
+        CreateGround();
+	}
+	
+	// Update is called once per frame
+	void CreateGround () {
+        Instantiate(ground, origin, Quaternion.identity);
+	}
+}
