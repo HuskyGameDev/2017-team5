1b0af35de658b3325e163cb0997978c4c8f1bd12
cwalker97 <collinw@mtu.edu>
2017-10-16 18:32:07 -0400

    commented code

--- Cow Quest - God Slayer/Assets/Scripts/CollisionDetection.cs
+++ Cow Quest - God Slayer/Assets/Scripts/CollisionDetection.cs
@@ -12,7 +12,8 @@ public class CollisionDetection : MonoBehaviour {
     bool enemyCollide = false;
     bool throwCollide = false;
     GameObject hero;
- 
+    //life counter for enemy and throwable collision
+    LifeCounter lc = new LifeCounter();
     // Use this for initialization
 	void Start () {
         hero = GameObject.FindGameObjectWithTag("Player");
@@ -21,9 +22,11 @@ public class CollisionDetection : MonoBehaviour {
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        //checks to see if the player is collising with an enemey.
         if (collision.gameObject.CompareTag("Enemy"))
         {
             enemyCollide = true;
+            lc.RemoveFromLives();
             //Debug.Log("The mummy hit you.");
         } else
         {
@@ -31,7 +34,9 @@ public class CollisionDetection : MonoBehaviour {
         }
         if (collision.gameObject.CompareTag("Throwables"))
         {
+           
             throwCollide = true;
+            lc.RemoveFromLives();
             //Debug.Log("Throw hit you.");
         } else
         {
