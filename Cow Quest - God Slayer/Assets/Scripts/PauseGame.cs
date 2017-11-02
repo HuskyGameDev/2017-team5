b037525bd71799cf5533b4946f43d2a8259fbc6e
emhovis <emhovis@mtu.edu>
2017-11-02 16:43:24 -0400

    Fully implemented pause funtionality
    removed exit function and replaced with pausing, which has been added as a prefab for easy instantiation

---
+++ Cow Quest - God Slayer/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform PauseMenu;
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Pause();
+		}
+	}
+	public void Pause(){
+		if (PauseMenu.gameObject.activeInHierarchy == false) {
+			Time.timeScale = 0;
+			PauseMenu.gameObject.SetActive (true);
+		} else {
+			PauseMenu.gameObject.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+}
