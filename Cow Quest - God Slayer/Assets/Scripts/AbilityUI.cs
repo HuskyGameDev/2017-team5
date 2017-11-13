45633d91c816421e8a9c83c0eb9bf5286f616c4c
emhovis <emhovis@mtu.edu>
2017-11-12 20:42:31 -0500

    abil bck images
    Ability bkgrnd images

--- Cow Quest - God Slayer/Assets/Scripts/AbilityUI.cs
+++ Cow Quest - God Slayer/Assets/Scripts/AbilityUI.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class AbilityUI : MonoBehaviour {
-	private string ability;
+	//private string ability;
 	public Text abilText;
 	public Transform lightSide;
 	public Transform darkSide;
-
+	public bool darkish = true;
 	void Start (){
-		bool darkish = true;
+		
 		abilText = GetComponent<Text>();
-		if (darkish == false) {
+		abilText.text = "SUPER STRIKE";
+		if (darkish == true) {
 			darkSide.gameObject.SetActive (true);
 			lightSide.gameObject.SetActive (false);
 		} else {
@@ -20,7 +21,7 @@ public class AbilityUI : MonoBehaviour {
 			lightSide.gameObject.SetActive (true);
 		}
 			
-		abilText.text = "SUPER STRIKE";
+
 	}
 		
 	void Update () {
