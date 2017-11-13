de4575c3067a725363cfa7ca34bd9be9bbf2207f
cwalker97 <collinw@mtu.edu>
2017-11-12 20:26:54 -0500

    Weapon System
    Scripts for the weapon system, along with a prefab weapon for throwables

---
+++ Cow Quest - God Slayer/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+    int enemyHealth = 0;
+    
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setEnemyHealth(int newHealth)
+    {
+        enemyHealth = newHealth;
+    }
+
+    public int getEnemyHealth()
+    {
+        return enemyHealth;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            setEnemyHealth(enemyHealth - 10);
+        }
+    }
+}
---
+++ Cow Quest - God Slayer/Assets/Scripts/ObjectGoBye.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGoBye : MonoBehaviour {
+    float delay = 5.0f;
+	// Use this for initialization
+	void Start () {
+       
+           
+               Destroy(gameObject, delay);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
