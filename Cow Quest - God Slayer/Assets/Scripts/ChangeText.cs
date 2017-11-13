244714feece461d6b6ead7daf91917c030891a4c
Catherine Steigerwald <ctsteige@mtu.edu>
2017-11-13 18:17:55 -0500

    Made Anubis text scene read text from file
    The Anubis text scene will now read text from a file line by line when WASD or the arrow keys are pressed

---
+++ Cow Quest - God Slayer/Assets/Scripts/ChangeText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Change the text in the text box whenever the player presses a key by reading the next line 
+ * from the text file */
+public class ChangeText : MonoBehaviour {
+    public Text text;
+    System.IO.StreamReader file = new System.IO.StreamReader("Assets/Text/anubis_dialogue.txt");
+    string line;
+
+	// Use this for initialization
+    /* Read the first line of the text file */
+	void Start () {
+        text.text = file.ReadLine();
+	}
+	
+	// Update is called once per frame
+    /* Move to the next line of text when WASD or the arroy keys are pressed */
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || 
+            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow)) {
+            if ((line = file.ReadLine()) != null) {
+                text.text = line;
+            }
+        }
+	}
+}
