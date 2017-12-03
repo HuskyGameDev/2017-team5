c81befb66807b362f9663e18fbbd78355a93b2e0
cwalker97 <collinw@mtu.edu>
2017-12-03 18:20:19 -0500

    enemy movement, needs fixing

--- Cow Quest - God Slayer/Assets/Scripts/AI.cs
+++ Cow Quest - God Slayer/Assets/Scripts/AI.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class AI : MonoBehaviour {
     private Rigidbody2D rb2d;
     public GameObject enemy;
-    public GameObject hero;
+    
     private float maxSpeed = 5f;
     private float moveForce = 350f;
 	// Use this for initialization
 	void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         enemy = GameObject.FindGameObjectWithTag("Enemy");
-        hero = GameObject.FindGameObjectWithTag("Hero");
-      bool detected = Detection();
+        
+      
 	}
 	
 	// Update is called once per frame
@@ -21,17 +21,7 @@ public class AI : MonoBehaviour {
 	
 	}
 
-    bool Detection()
-    {
-        if (Mathf.Abs(hero.transform.position.x - enemy.transform.position.x) < 30)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+   
 
 
 
--- Cow Quest - God Slayer/Assets/Scripts/EMove.cs
+++ Cow Quest - God Slayer/Assets/Scripts/EMove.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EMove : MonoBehaviour {
-    GameObject enemy = GameObject.FindGameObjectWithTag("Enemy");
+    GameObject enemy;
+    GameObject[] enemiesArray;
+    public GameObject hero;
+   
     private float moveForce = 350f;
     private float maxSpeed = 5f;
     private bool moveRight = true;
     private bool moveLeft = false;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        hero = GameObject.FindGameObjectWithTag("Player");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,13 +23,140 @@ public class EMove : MonoBehaviour {
 
     private void Move()
     {
-        if(true)
+        // FindEnemies(30);
+        if (gameObject.CompareTag("Cat"))
         {
-            enemy.transform.Translate(new Vector2(maxSpeed, 0) * Time.deltaTime);
+            enemy = GameObject.FindGameObjectWithTag("Cat");
+            Vector3 pointA = enemy.transform.position;
+            Vector3 pointB = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
+            if (enemy.transform.position.x < pointB.x)
+            {
+                enemy.transform.Translate(new Vector3(1, 0, 0) * maxSpeed * Time.deltaTime);
+            }
+            else if (enemy.transform.position.x > pointA.x)
+            {
+                enemy.transform.Translate(new Vector3(-1, 0, 0) * maxSpeed * Time.deltaTime);
+            }
+
+        } else if (gameObject.CompareTag("Dog"))
+        {
+            enemy = GameObject.FindGameObjectWithTag("Dog");
+            Vector3 pointA = enemy.transform.position;
+            Vector3 pointB = new Vector3(transform.position.x + 5, transform.position.y, transform.position.z);
+
+            if (Detection())
+            {
+                if (Direction())
+                {
+
+                    enemy.transform.Translate(new Vector3(-1, 0, 0) * maxSpeed * Time.deltaTime);
+
+                }
+                else
+                {
+                    enemy.transform.Translate(new Vector3(1, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+            } else
+            {
+                if(enemy.transform.position.x < pointB.x)
+                {
+                    enemy.transform.Translate(new Vector3(1, 0, 0) * maxSpeed * Time.deltaTime);
+                } else if( enemy.transform.position.x > pointA.x)
+                {
+                    enemy.transform.Translate(new Vector3(-1, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+            }
+        } else if (gameObject.CompareTag("Falcon"))
+        {
+            enemy = GameObject.FindGameObjectWithTag("Falcon");
+
+            Vector3 pointA = enemy.transform.position;
+            Vector3 pointB = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
+            if (enemy.transform.position.y < pointB.y)
+            {
+                enemy.transform.Translate(new Vector3(0, 1, 0) * maxSpeed * Time.deltaTime);
+            }
+            else if (enemy.transform.position.y > pointA.y)
+            {
+                enemy.transform.Translate(new Vector3(0, -1, 0) * maxSpeed * Time.deltaTime);
+            }
+
+
+        } else if (gameObject.CompareTag("Mummy"))
+        {
+            enemy = GameObject.FindGameObjectWithTag("Mummy");
+            if (Detection())
+            {
+                if (Direction())
+                {
+
+                    enemy.transform.Translate( new Vector3(-1, 0, 0) * maxSpeed * Time.deltaTime);
+
+                }
+                else
+                {
+                    enemy.transform.Translate(new Vector3(1, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+            }
+        } else if (gameObject.CompareTag("Pharoh"))
+        {
+            enemy = GameObject.FindGameObjectWithTag("Pharoh");
+            if (Detection())
+            {
+                if (Direction())
+                {
+
+                    enemy.transform.Translate(new Vector3(-1, 0, 0) * maxSpeed * Time.deltaTime);
+
+                }
+                else
+                {
+                    enemy.transform.Translate(new Vector3(1, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    //  GameObject[] FindEnemies(int l)
+    // {
+    //    enemiesArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+    //  var enemyList = new System.Collections.Generic.List<GameObject>();
+    // for(int i = 0; i < enemiesArray.Length; i++)
+    //{
+    //   if(enemiesArray[i].layer == l)
+    //  {
+    //     enemyList.Add(enemiesArray[i]);
+    //
+    //            }
+    //      }
+    //    if(enemyList.Count == 0)
+    //  {
+    //    return null;
+    //}
+    //return enemyList.ToArray();
+    //   }
+
+    bool Detection()
+    {
+        if (Vector3.Distance(transform.position, hero.transform.position) < 30)
+        {
+            return true;
         }
         else
         {
-            enemy.transform.Translate(new Vector2(-1 * maxSpeed, 0) * Time.deltaTime);
+            return false;
+        }
+    }
+
+    bool Direction()
+    {
+        if(hero.transform.position.x - enemy.transform.position.x < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
