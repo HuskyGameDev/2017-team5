a51ae0abcc5375da5887c2e3726102f6a281b8f9
smicklas <smicklas@mtu.edu>
2017-12-13 13:45:55 -0600

    Fine tuning of game for final submission
    Removed movement from Cats
    Added movement distance to mummies
    Moved teleporter location for light level 1
    Increased jumping force for light level 1
    Changed name of powers for Light and Dark level 1
    Moved starting point of Dark Level 1 to 0, 0
    Flipped image for text with Anubis
    Added TBC teleporter to end of Light Level 1

--- Cow Quest - God Slayer/Assets/Scripts/ChangeText.cs
+++ Cow Quest - God Slayer/Assets/Scripts/ChangeText.cs
@@ -6,32 +6,38 @@ using UnityEngine.SceneManagement;
 
 /* Change the text in the text box whenever the player presses a key by reading the next line 
  * from the text file */
-public class ChangeText : MonoBehaviour {
-    public Text text;               // The text object to be modified on screen
-    public string filename;         // Name of the file to read the text from
-    public string sceneToChangeTo;  // Scene to change to after all the text in the file has been read
-    System.IO.StreamReader file;
-    string line;                    // Current line in the file being read
+public class ChangeText : MonoBehaviour
+{
+	public Text text;
+	// The text object to be modified on screen
+	static string filename;
+	// Name of the file to read the text from
+	public string sceneToChangeTo;
+	// Scene to change to after all the text in the file has been read
+	System.IO.StreamReader file;
+	string line;
+	// Current line in the file being read
 
 	// Use this for initialization
-	void Start () {
-        /* Read the first line of the text file */
-        file = new System.IO.StreamReader(filename);
-        text.text = file.ReadLine();
+	void Start ()
+	{
+		/* Read the first line of the text file */
+		file = new System.IO.StreamReader (filename);
+		text.text = file.ReadLine ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        /* Move to the next line of text when WASD, the arrow keys, or the left mouse button are pressed */
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || 
-            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow)
-            || Input.GetMouseButtonDown(0)) {
-            if ((line = file.ReadLine()) != null) {
-                text.text = line;
-            }
-            else { /* After the last line of the text file has been read, change to the next scene */
-                SceneManager.LoadScene(sceneToChangeTo);
-            }
-        }
+	void Update ()
+	{
+		/* Move to the next line of text when WASD, the arrow keys, or the left mouse button are pressed */
+		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D) ||
+		    Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.RightArrow)
+		    || Input.GetMouseButtonDown (0)) {
+			if ((line = file.ReadLine ()) != null) {
+				text.text = line;
+			} else { /* After the last line of the text file has been read, change to the next scene */
+				SceneManager.LoadScene (sceneToChangeTo);
+			}
+		}
 	}
 }
