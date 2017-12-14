cb446044958808d7298eba53850c842abe6d0384
Catherine Steigerwald <ctsteige@mtu.edu>
2017-12-14 13:57:59 -0500

    Fixed text system so that it works with .exe
    Fixed the text system so that the text files can still be read when the game is exported to a .exe

--- Cow Quest - God Slayer/Assets/Scripts/ChangeText.cs
+++ Cow Quest - God Slayer/Assets/Scripts/ChangeText.cs
@@ -1,41 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-/* Change the text in the text box whenever the player presses a key by reading the next line 
- * from the text file */
+/* Change the text in the text box whenever the player presses WASD, the arrow keys, the spacebar,
+ *  or the left mouse button by reading the next line from the provided text file */
 public class ChangeText : MonoBehaviour
 {
-	public Text text;
-	// The text object to be modified on screen
-	static string filename;
-	// Name of the file to read the text from
-	public string sceneToChangeTo;
-	// Scene to change to after all the text in the file has been read
-	System.IO.StreamReader file;
-	string line;
-	// Current line in the file being read
+    // The text object to be modified in the text box on screen
+    public Text textObject;
+
+    // File to read the text from
+    public TextAsset textFile;
+
+    // Scene to change to after all the text in the file has been read
+    public string sceneToChangeTo;
+
+    // Array to store the lines of the text file being read
+    string[] fileLines;
+
+    // Total number of lines in the text file being read
+    int numOfLines;
+
+    // Line number of the next line to be read
+    int nextLine;
 
 	// Use this for initialization
 	void Start ()
 	{
-		/* Read the first line of the text file */
-		file = new System.IO.StreamReader (filename);
-		text.text = file.ReadLine ();
-	}
+        /* Split the lines of the text file at every newline character */
+        string fileText = textFile.text;
+        fileLines = Regex.Split(fileText, "\r\n|\n|\r");
+        numOfLines = fileLines.Length;
+
+        /* Read the first line of the text file */
+        nextLine = 0;
+        textObject.text = fileLines[nextLine++];
+    }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		/* Move to the next line of text when WASD, the arrow keys, or the left mouse button are pressed */
+		/* Move to the next line of text when WASD, the arrow keys, the spacebar, or the left mouse button are pressed */
 		if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A) || Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D) ||
 		    Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.LeftArrow) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.RightArrow)
-		    || Input.GetMouseButtonDown (0)) {
-			if ((line = file.ReadLine ()) != null) {
-				text.text = line;
-			} else { /* After the last line of the text file has been read, change to the next scene */
+		    || Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0))
+        {
+            if (nextLine < numOfLines)
+            {
+                textObject.text = fileLines[nextLine++];
+            } else { /* After the last line of the text file has been read, change to the next scene */
 				SceneManager.LoadScene (sceneToChangeTo);
 			}
 		}
--- Cow Quest - God Slayer/Assets/Scripts/ChangeText_Buttons.cs
+++ Cow Quest - God Slayer/Assets/Scripts/ChangeText_Buttons.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-/* Change the text in the text box whenever the player presses a key by reading the next line 
- * from the text file, and display buttons after the appropriate line has been read */
+/* Change the text in the text box whenever the player presses WASD, the arrow keys, the space bar, or the left mouse button
+ * by reading the next line from the provided text file, and display buttons after the appropriate line has been read */
 public class ChangeText_Buttons : MonoBehaviour {
-    public Text text;               // The text object to be modified on screen
-    public string filename;         // Name of the file to read the text from
+    public Text textObject;               // The text object to be modified in the text box on screen
+    public TextAsset textFile;      // File to read the text from
     public string sceneChangeYes;   // Scene to change to if the player selects the "Yes" option
     public string sceneChangeNo;    // Scene to change to if the player selects the "No" option
 
@@ -16,13 +17,15 @@ public class ChangeText_Buttons : MonoBehaviour {
     public GameObject yesButton;    // Buttons must be defined as GameObject types to use the SetActive() function
     public GameObject noButton;
     Button firstBtn;                // Buttons must also be defined as Button types to use the onClick() function
-    Button yesBtn; Button noBtn;    
+    Button yesBtn; Button noBtn;
 
-    System.IO.StreamReader file;
-    string line;                    // Current line in the file being read
-    int linesRead = 1;              // Number of lines that have been read
+
+    string[] fileLines; // Array to store the lines of the text file being read
+    int numOfLines;     // Total number of lines in the text file being read
+    int nextLine;       // Line number of the next line to be read
+ 
     int linesUntilFirstButton;      // Number of lines of text until the first button should be displayed
-    int linesUntilYesNo;            // Number of lines of text until the Yes and No buttons should be displayed; does not include the first two lines containing integers
+    int linesUntilYesNo;            // Number of lines of text until the Yes and No buttons should be displayed
     //int linesOfNoText;            // Number of lines of the "No" response text
 
 	// Use this for initialization
@@ -43,37 +46,42 @@ public class ChangeText_Buttons : MonoBehaviour {
         yesButton.SetActive(false);
         noButton.SetActive(false);
 
+        /* Split the lines of the text file at every newline character */
+        string fileText = textFile.text;
+        fileLines = Regex.Split(fileText, "\r\n|\n|\r");
+        numOfLines = fileLines.Length;
+
         /* Read the first two lines of the text file to get the number of lines until the buttons should appear */
-        file = new System.IO.StreamReader(filename);
-        linesUntilFirstButton = int.Parse(file.ReadLine());
-        linesUntilYesNo = int.Parse(file.ReadLine());
+        nextLine = 0;
+        linesUntilFirstButton = int.Parse(fileLines[nextLine++]);
+        linesUntilYesNo = int.Parse(fileLines[nextLine++]);
+        print("linesUntilFirstButton = " + linesUntilFirstButton + ", linesUntilYesNo = " + linesUntilYesNo);
 
         ///* Read the second line of the text file to get the number of lines between the "Yes" and "No" response text */
         //linesOfNoText = int.Parse(file.ReadLine());
 
         /* Read the first line of text to display in the on-screen text box */
-        text.text = file.ReadLine();
+        textObject.text = fileLines[nextLine++];
     }
 	
 	// Update is called once per frame
-    /* Move to the next line of text when WASD, the arrow keys, or the left mouse button are pressed */
+    /* Move to the next line of text when WASD, the arrow keys, the spacebar, or the left mouse button are pressed */
 	void Update () {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) ||
-            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow) 
-            || Input.GetMouseButtonDown(0)) {
+            Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow)
+            || Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
             /* If the appropriate line of text has been reached, display the first button */
-            if (linesRead == linesUntilFirstButton) {
+            if (nextLine == linesUntilFirstButton) {
                 firstButton.SetActive(true);
             }
             /* If the appropriate line of text has been reached, display the Yes and No buttons */
-            else if (linesRead == linesUntilYesNo) {
+            else if (nextLine == linesUntilYesNo) {
                 yesButton.SetActive(true);
                 noButton.SetActive(true);              
             }
             else { /* Move to the next line of text */
-                if ((line = file.ReadLine()) != null) {
-                    text.text = line;
-                    linesRead++;
+                if (nextLine < numOfLines) {
+                    textObject.text = fileLines[nextLine++];
                 }
                 else { /* After the last line of the text file has been read, change to the next scene */
                     SceneManager.LoadScene(sceneChangeYes);
@@ -83,18 +91,14 @@ public class ChangeText_Buttons : MonoBehaviour {
 	}
 
     /* Continue reading through text for the current scene */
-    void ContinueText()
-    {
+    void ContinueText() {
         firstButton.SetActive(false);
 
         /* Read the next line of text */
-        if ((line = file.ReadLine()) != null)
-        {
-            text.text = line;
-            linesRead++;
+        if (nextLine < numOfLines) {
+            textObject.text = fileLines[nextLine++];
         }
-        else
-        { /* After the last line of the text file has been read, change to the next scene */
+        else { /* After the last line of the text file has been read, change to the next scene */
             SceneManager.LoadScene(sceneChangeYes);
         }
     }
@@ -109,12 +113,11 @@ public class ChangeText_Buttons : MonoBehaviour {
         //    file.ReadLine();
         //    linesRead++;
         //
-
         ///* If there was no text for the "No" option, read the first line of the text for the "Yes" option */
+ 
         /* Read the first line of the text for the "Yes" option */
-        if ((line = file.ReadLine()) != null) {
-            text.text = line;
-            linesRead++;
+        if (nextLine < numOfLines) {
+            textObject.text = fileLines[nextLine++];
         }
         else { /* After the last line of the text file has been read, change to the next scene */
               SceneManager.LoadScene(sceneChangeYes);
