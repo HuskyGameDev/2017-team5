a617b3114fe6139979e31f283d7535f33d6b9b37
Brady Bilderback <diytto@diytto.com>
2018-02-25 21:56:33 -0500

    Updated UI elements
    Updated UI elements

--- Cow Quest - God Slayer/Assets/Scripts/PauseGame.cs
+++ Cow Quest - God Slayer/Assets/Scripts/PauseGame.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PauseGame : MonoBehaviour {
 	public Transform PauseMenu;
 
-
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Escape)) {
+	void Update() 
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+		{
 			Pause();
 		}
 	}
-	public void Pause(){
-		if (PauseMenu.gameObject.activeInHierarchy == false) {
+	
+	public void Pause()
+	{
+		if (PauseMenu.gameObject.activeInHierarchy == false) 
+		{
 			Time.timeScale = 0;
 			PauseMenu.gameObject.SetActive (true);
-		} else {
+		} 
+		else 
+		{
 			PauseMenu.gameObject.SetActive (false);
 			Time.timeScale = 1;
 		}
