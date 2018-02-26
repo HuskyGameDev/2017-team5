ab7c81e5065b157bf97387f711ab7cdf9c5a2af2
cwalker97 <collinw@mtu.edu>
2018-02-25 21:28:36 -0500

    Platforms now lit.

---
+++ Cow Quest - God Slayer/Assets/Scripts/InvisibleBeSeen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleBeSeen : MonoBehaviour {
+    GameObject hero;
+    SpriteRenderer spriteRenderer;
+    
+    public bool inRange()
+    {
+        if(Vector3.Distance(transform.position, hero.transform.position) < 5)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    private void Start()
+    {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        hero = GameObject.FindGameObjectWithTag("Player");
+        this.spriteRenderer.enabled = false;
+    }
+    private void Update()
+    {
+        checkRange();
+    }
+
+    public void checkRange()
+    {
+        if(inRange() && Input.GetKeyDown(KeyCode.F))
+        {
+            getLit();
+        }
+        
+    }
+
+    public void getLit()
+    {
+        Debug.Log("Lit");
+        this.spriteRenderer.enabled = true;
+    }
+
+}
