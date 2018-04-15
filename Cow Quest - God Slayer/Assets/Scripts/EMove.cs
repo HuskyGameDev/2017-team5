9d7ee5613470ec58a29d0132b428b8a5e5282d55
cwalker97 <collinw@mtu.edu>
2018-04-15 17:04:18 -0400

    made some boss changes

--- Cow Quest - God Slayer/Assets/Scripts/EMove.cs
+++ Cow Quest - God Slayer/Assets/Scripts/EMove.cs
@@ -35,7 +35,7 @@ public class EMove : MonoBehaviour {
     {
         keepUpright(this.gameObject);
 
-        if (this.gameObject.CompareTag("Cat") || this.gameObject.CompareTag("BigBoy") || this.gameObject.CompareTag("EndDude"))
+        if (this.gameObject.CompareTag("Cat") || this.gameObject.CompareTag("BigBoy"))
         {
 
 
@@ -162,7 +162,7 @@ public class EMove : MonoBehaviour {
             }
 		} else if (this.gameObject.CompareTag("Pharoh"))
         {
-            if (Detection())
+            if (bossDetection())
             {
 				if (Direction(this.gameObject))
                 {
@@ -176,11 +176,73 @@ public class EMove : MonoBehaviour {
                 }
             }
         }
-    }
+        else if (this.gameObject.CompareTag("EndDude"))
+        {
+            if (bossDetection())
+            {
+                if (Direction(this.gameObject))
+                {
 
+                    transform.Translate(new Vector3(-.5f, 0, 0) * maxSpeed * Time.deltaTime);
+
+                }
+                else
+                {
+                    transform.Translate(new Vector3(.5f, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+            } else
+            {
+                if (!set)
+                {
+                    pointA = transform.position;
+                    set = true;
+                }
+                Vector3 pointB = new Vector3(pointA.x + distMove, pointA.y, pointA.z);
+
+                startset = true;
+
+
+
+
+
+                if (transform.position.x > pointB.x)
+                {
+                    moveRight = false;
+
+                }
+                else if (transform.position.x < pointA.x)
+                {
+                    moveRight = true;
+
+                }
+                if (moveRight)
+                {
+                    transform.Translate(new Vector3(.1f, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    transform.Translate(new Vector3(-.1f, 0, 0) * maxSpeed * Time.deltaTime);
+                }
+
+            }
+        }
+    }
+    
     bool Detection()
     {
         if (Vector3.Distance(transform.position, hero.transform.position) < 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool bossDetection()
+    {
+        if (Vector3.Distance(transform.position, hero.transform.position) < 20)
         {
             return true;
         }
